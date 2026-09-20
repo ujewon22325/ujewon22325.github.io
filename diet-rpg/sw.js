d8123d8f3e377bd84447f7d940eef7d1eba7b63e
ujewon22325 <benjamin1121123@gmail.com>
2026-09-20 16:06:39 +0900

    Cache Diet RPG PNG icons

--- diet-rpg/sw.js
+++ diet-rpg/sw.js
@@ -1,5 +1,5 @@
-const CACHE='diet-rpg-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='diet-rpg-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{const c=n.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return n}).catch(()=>caches.match('./index.html'))))});
