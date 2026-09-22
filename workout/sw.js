588f3460b9b960f9323d191f7968bc9aed798c2f
ujewon22325 <benjamin1121123@gmail.com>
2026-09-22 19:43:27 +0900

    Refresh workout PWA cache for exercise search links

--- workout/sw.js
+++ workout/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='workout-note-pwa-';
-const CACHE=CACHE_PREFIX+'v6';
+const CACHE=CACHE_PREFIX+'v7-youtube';
 const CORE=['./','./index.html','./manifest.webmanifest','../icons/icon-192.png','../icons/icon-512.png'];
 
 self.addEventListener('install',e=>{
